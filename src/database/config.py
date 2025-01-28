3e2641760354a64bb29e45dba2baad1fb1e257ff
ItaySagy123 <itay.sagy@fairgen.ai>
2025-01-28 23:18:49 +0200

    .env implemented

--- src/database/config.py
+++ src/database/config.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from src.config.config import CONFIG
 
 
-engine = create_engine('postgresql+psycopg2://postgres:postgres@postgres:5432/moveo_db')
+engine = create_engine(f'postgresql+psycopg2://{CONFIG["db"]["db_user"]}:{CONFIG["db"]["db_password"]}@{CONFIG["db"]["db_host"]}:{CONFIG["db"]["db_port"]}/{CONFIG["db"]["db_name"]}')
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
